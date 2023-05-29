41ddae2f9ee51f172bee4841e8a0748f27e1147b
BeatrizLandi <beatriz.landi.coelho@gmail.com>
2023-05-29 06:04:14 -0300

    a

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@
 #framework
 from flask import Flask, jsonify, make_response, request
 #parcial imports
-from controller_sender import cheek_if_email_exist,send_email_gmail_com
+from controller_sender import cheek_if_email_exist,send_email_gmail_com, generic_send_email_gmail_com
 
 #instance this baby
 app = Flask(__name__)
@@ -39,6 +39,32 @@ def email_cheker():
 #________________________________________________________________
 
 
+@app.route("/v1/emailsendgeneric", methods=['POST'])
+def email_sender_generic_version():
+      
+      company_email = request.json['company email']
+      company_email_key = request.json['company email key']
+      tittle_email = request.json['title']
+      user_email = request.json['email']
+      body_email = request.json['body']
+
+
+      generic_send_email_gmail_com(company_email,company_email_key,tittle_email,user_email,body_email)
+  
+      return make_response(
+      jsonify(
+
+        comp_email=company_email,
+        tittle=tittle_email,
+        body= body_email,
+        to=user_email,
+        status = 200
+  
+      )
+    )   
+    
+  
+#________________________________________________________________
 @app.route("/v2/emailsend", methods=['POST'])
 def email_sender():
       
@@ -60,11 +86,7 @@ def email_sender():
       )
     )
   
-  
-    
-
 #________________________________________________________________
-
 
 if __name__ == '__main__':
 
--- app/controller_sender.py
+++ app/controller_sender.py
@@ -27,8 +27,23 @@ def send_email_gmail_com(email_title,user_email, body_email):
     company_email_key = "wykxeptrluwazmvr"
 
     # Instanciar a classe EmailSender
-    email_sender = EmailSender('smtp.gmail.com', 587, 'emailtesteremailtester123@gmail.com', 'wykxeptrluwazmvr')
+    email_sender = EmailSender('smtp.gmail.com', 587, company_email, company_email_key)
+
+    # Enviar um email
+    email_sender.send_email("title", user_email, body_email)
+
+#exammple
+#send_email_gmail_com("email tittle","andressa.fideliscoelho@gmail.com","email body")
+#______________________________________________________________
+
+def generic_send_email_gmail_com(company_email,company_email_key,email_title, user_email, body_email):
+   
+    # Instanciar a classe EmailSender
+    email_sender = EmailSender('smtp.gmail.com', 587, company_email, company_email_key)
 
     # Enviar um email
     email_sender.send_email(email_title, user_email, body_email)
 
+#example
+#generic_send_email_gmail_com("emailtesteremailtester123@gmail.com","wykxeptrluwazmvr","email tittle","andressa.fideliscoelho@gmail.com","email body")
+#______________________________________________________________
