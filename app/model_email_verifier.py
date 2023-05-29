d81fb5d5d04602267d4985074d76d214d3c7d469
BeatrizLandi <beatriz.landi.coelho@gmail.com>
2023-05-28 21:57:17 -0300

    a

--- app/model_email_verifier.py
+++ app/model_email_verifier.py
@@ -3,21 +3,23 @@
 
 from validate_email import validate_email
 
-def email_cheker(email):
-
-
-    try:    
-        
-        if(validate_email(email)==True):
-            status = "a"
-        else:
-            status = "b"
-           
-        return True
-
+def email_checker(email):
+    try:
+        validate_email(email)
+        validate_status = True
     except Exception as e:
-
-        return False 
+        validate_status = False
     
-#exaple    
-email_cheker("beatriz.landi.coelho@gmail.com")
+    email_checker_status = True  # Defina o status da função email_checker como True
+    
+    return validate_status, email_checker_status
+
+# Chamada da função e recebimento dos valores retornados
+#validate_result, email_checker_result = email_checker("beatriz.landi.coelho@gmail.com")
+
+#print(validate_result)          # Status retornado pela função validate_email
+#print(email_checker_result)     # Status retornado pela função email_checker
+
+#exaple
+#email_checker("beatriz.landi.coelho@gmail.com")
+
